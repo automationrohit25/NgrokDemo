020223547e3847806bfe81e1485ad44cd103a5b3
rohit <rohit25.infy@gmail.com>
2021-07-11 03:15:26 +0530

    tests

---
+++ src/test/java/dataProviderExcel/DataProviderIntegrationExcel.java
@@ -0,0 +1,14 @@
+package dataProviderExcel;
+
+import java.io.FileInputStream;
+
+
+import org.testng.annotations.Test;
+
+public class DataProviderIntegrationExcel {
+
+	@Test
+	public void m2() {
+		System.out.println("hello world");
+	}
+}
---
+++ src/test/java/dataProviderExcel/Hello.java
@@ -0,0 +1,15 @@
+package dataProviderExcel;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+		
+		@Test
+		public void m1(){
+			System.out.println("ngrokz");
+		}
+		
+
+	}
+
+
